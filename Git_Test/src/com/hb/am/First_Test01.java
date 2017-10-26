709a32667f5ed3729bb8b5af6f4bd6b6f7de1829
5CLASS-184 <5CLASS-184@Hb03-05>
2017-10-26 10:34:02 +0900

    주석처리

--- Git_Test/src/com/hb/am/First_Test01.java
+++ Git_Test/src/com/hb/am/First_Test01.java
@@ -1,7 +1,11 @@
 package com.hb.am;
 
 public class First_Test01 {
+	// 메인 메소드; 시작점, JVM이 호출
 	public static void main(String[] args) {
+		// 모니터(화면)에 출력하는 명령어
 		System.out.println("첫번째 예제");
+		
+		System.out.println("이름 : 홍길동");
 	}
 }
